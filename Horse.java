f325258881503cbd6273333cbf0d4f423ddd0504
Bryan Mayea <98754849+BryanMayea@users.noreply.github.com>
2022-10-10 01:32:40 +0000

    Solution

--- Horse.java
+++ Horse.java
@@ -1,1 +1,29 @@
 //Create the Horse class here.
+
+class Horse{
+
+  private String name;
+  private Double weight;
+  private Boolean tame;
+
+public Horse (String name, Double weight, Boolean tame){
+  
+  this.name = name;
+  this.weight = weight;
+  this.tame = tame;
+  
+}
+
+public String horseName(){
+  return this.name;
+}
+
+public Double horseWeight(){
+  return this.weight;
+}
+
+public Boolean horseTame(){
+  return this.tame;
+}
+  
+}
